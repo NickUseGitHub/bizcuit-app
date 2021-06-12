381e62374d8cc9e5211247fa1f7b7c0c4371b6a6
Chaiwtun <chaiwtun@scg.com>
2021-06-13 00:40:29 +0700

    [main] seed data every init project

--- backend/src/beer/beer.service.ts
+++ backend/src/beer/beer.service.ts
@@ -1,3 +1,9 @@
+import {
+  company as fakerCompany,
+  date as fakerDate,
+  name as fakerName,
+  datatype as fakerDatatype,
+} from 'faker';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -10,10 +16,10 @@ export class BeerService implements OnModuleInit {
   constructor(
     private configService: ConfigService,
     @InjectRepository(Beer)
-    private readonly ticketsRepository: Repository<Beer>,
+    private readonly beersRepository: Repository<Beer>,
   ) {}
 
-  private async seedData(): Promise<void> {
+  private async seedData(): Promise<Beer[]> {
     const isSeedData = this.configService.get<string>('SEED_DATA') || '';
     const nodeEnv = this.configService.get<string>('NODE_ENV');
 
@@ -21,11 +27,37 @@ export class BeerService implements OnModuleInit {
       isSeedData.toUpperCase() === 'YES' && nodeEnv !== 'production';
 
     if (shouldSeedData !== true) {
-      return;
+      return [];
     }
 
-    console.log('isSeedData', isSeedData);
-    console.log('nodeEnv', nodeEnv);
+    const amountDataForSeed = 10;
+    const fakedBeers: Beer[] = [...new Array(amountDataForSeed)].map(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      function getSeedBeerData(_, index: number): Beer {
+        const beer = new Beer();
+
+        beer.uid = fakerDatatype.uuid();
+        beer.brand = fakerCompany.companyName();
+        beer.name = fakerName.firstName();
+        beer.style = 'style';
+        beer.hop = 'hop';
+        beer.yeast = 'yeast';
+        beer.malts = 'malts';
+        beer.ibu = 'ibu';
+        beer.alcohol = 'alcohol';
+        beer.blg = 'blg';
+        beer.createdAt = fakerDate.past();
+        beer.updatedAt = fakerDate.past();
+
+        return beer;
+      },
+    );
+
+    await Promise.all(
+      fakedBeers.map((fakeBeer) => this.beersRepository.save(fakeBeer)),
+    );
+
+    return fakedBeers;
   }
 
   onModuleInit() {
