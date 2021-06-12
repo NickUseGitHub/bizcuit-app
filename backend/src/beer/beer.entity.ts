e1ba0083d1606723c6ea0703cdc86a4da1dc4a2a
Chaiwtun <chaiwtun@scg.com>
2021-06-12 20:58:45 +0700

    [main] add Beer entity

---
+++ backend/src/beer/beer.entity.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Beer {
+  @PrimaryGeneratedColumn('uuid')
+  uid: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  brand: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  style: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  hop: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  yeast: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  malts: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  ibu: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  alcohol: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  blg: string;
+
+  @Column({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamptz' })
+  updatedAt: Date;
+}
