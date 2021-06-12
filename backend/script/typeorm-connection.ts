c11e74fbd2417f237a17c47e8c6b2fb7c29d62d1
Chaiwtun <chaiwtun@scg.com>
2021-06-13 00:05:18 +0700

    [main] fix env files

--- backend/script/typeorm-connection.ts
+++ backend/script/typeorm-connection.ts
@@ -1,8 +1,12 @@
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { getBackendEnv } from '../src/config/database.config';
 
+const envPostFix = `.${process.env.NODE_ENV || 'example'}`;
+const envFile = `../.env${envPostFix}`;
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, envFile) });
 
 export default {
   ...getBackendEnv(),
