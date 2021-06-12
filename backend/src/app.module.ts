2c8bb055d4d387f8b27dca02e535e03285d2bc7b
Chaiwtun <chaiwtun@scg.com>
2021-06-12 20:17:00 +0700

    [main] backend read .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BeerController } from './beer/beer.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.example'],
+    }),
+  ],
   controllers: [AppController, BeerController],
   providers: [AppService],
 })
