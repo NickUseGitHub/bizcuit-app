4f4cfd69beff8c35cd33081f2b16710bd7b82202
Chaiwtun <chaiwtun@scg.com>
2021-06-12 22:41:51 +0700

    [main] initial db when docker compose up

---
+++ backend/migration/1623511530071-InitMigration.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitMigration1623511530071 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
