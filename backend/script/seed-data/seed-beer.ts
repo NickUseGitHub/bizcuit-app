d4c26f36152b1273698375a4c0dea89ed4967674
Chaiwtun <chaiwtun@scg.com>
2021-06-13 21:37:31 +0700

    [main] fix bug close connection pool before seed data

--- backend/script/seed-data/seed-beer.ts
+++ backend/script/seed-data/seed-beer.ts
@@ -1,14 +1,12 @@
+import { Connection, Repository } from 'typeorm';
 import { Beer } from '../../src/beer/beer.entity';
 import { BeerService } from '../../src/beer/beer.service';
-import { Connection, Repository } from 'typeorm';
 import { Seedable } from './types';
 
 export class SeedBeer implements Seedable {
-  private connection: Connection;
   private entityRepository: Repository<Beer>;
 
   constructor(connection: Connection) {
-    this.connection = connection;
     this.entityRepository = connection.getRepository(Beer);
   }
 
@@ -18,7 +16,7 @@ export class SeedBeer implements Seedable {
       [...new Array(amountDataForSeed)].map(
         async function getFakeBeer(): Promise<Beer> {
           const fakeBeer = await BeerService.getRandomBeerFromThirdParty();
-          this.entityRepository.save(fakeBeer);
+          await this.entityRepository.save(fakeBeer);
 
           return fakeBeer;
         }.bind(this),
