94192a4544aa359d25b0e2d6f86f42a037ef8bdc
Chaiwtun <chaiwtun@scg.com>
2021-06-13 17:05:28 +0700

    [main] create beerService

--- frontend/services/beer.service.ts
+++ frontend/services/beer.service.ts
@@ -2,8 +2,14 @@ import { Beer } from '@backend/beer/beer.entity'
 import { ApiService } from '@services/api.service'
 
 class BeerService extends ApiService {
-  async getRandomBeer(): Promise<Beer | null> {
-    return this.get('/beer/random').then((res) => res.data)
+  async getRandomBeer(delaySecond: number = 0): Promise<Beer | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        this.get('/beer/random')
+          .then((res) => resolve(res.data))
+          .catch(reject)
+      }, delaySecond * 1000)
+    })
   }
 }
 
