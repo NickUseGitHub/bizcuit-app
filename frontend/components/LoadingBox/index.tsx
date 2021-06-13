245b662f18d0946f11523509443abc8f906a03a7
Chaiwtun <chaiwtun@scg.com>
2021-06-13 17:04:56 +0700

    [main] create loading component

---
+++ frontend/components/LoadingBox/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import styles from './index.module.css'
+
+export default function LoadingBox() {
+  return (
+    <div>
+      <h1 className="flex justify-between items-center">
+        <svg
+          className={styles.clockLoading}
+          version="1.1"
+          id="L2"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+          enable-background="new 0 0 100 100"
+        >
+          <circle
+            fill="none"
+            stroke="#000"
+            stroke-width="4"
+            stroke-miterlimit="10"
+            cx="50"
+            cy="50"
+            r="48"
+          />
+          <line
+            fill="none"
+            stroke-linecap="round"
+            stroke="#000"
+            stroke-width="4"
+            stroke-miterlimit="10"
+            x1="50"
+            y1="50"
+            x2="85"
+            y2="50.5"
+          >
+            <animateTransform
+              attributeName="transform"
+              dur="2s"
+              type="rotate"
+              from="0 50 50"
+              to="360 50 50"
+              repeatCount="indefinite"
+            />
+          </line>
+          <line
+            fill="none"
+            stroke-linecap="round"
+            stroke="#000"
+            stroke-width="4"
+            stroke-miterlimit="10"
+            x1="50"
+            y1="50"
+            x2="49.5"
+            y2="74"
+          >
+            <animateTransform
+              attributeName="transform"
+              dur="15s"
+              type="rotate"
+              from="0 50 50"
+              to="360 50 50"
+              repeatCount="indefinite"
+            />
+          </line>
+        </svg>
+        <span className="ml-2">Be chilled... We are getting your beer.</span>
+      </h1>
+    </div>
+  )
+}
