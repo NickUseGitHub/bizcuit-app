3ea63ef6f95c028658176f45c70c6dcf11a683b2
Chaiwtun <chaiwtun@scg.com>
2021-06-13 15:59:12 +0700

    [main] allow cors for localhost

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -7,6 +8,10 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  } as CorsOptions);
+
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
