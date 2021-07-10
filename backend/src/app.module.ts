251c27be96c7a55530116538371141c1a7d19e78
Chaiwtun <chaiwtun@scg.com>
2021-07-10 21:06:26 +0700

    [main] init module auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { BeerModule } from '~beer/beer.module';
 import { VinesModule } from '~vine/vines.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BeerModule, VinesModule],
+  imports: [BeerModule, VinesModule, AuthModule],
   providers: [],
 })
 export class AppModule {}
