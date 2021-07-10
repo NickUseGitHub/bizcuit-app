858626c4f3ebdb1ae276a92cdeacb006977142da
Chaiwtun <chaiwtun@scg.com>
2021-07-10 22:23:00 +0700

    [main] use cookie-parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,15 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.use(cookieParser());
   app.setGlobalPrefix('api/v1');
   app.enableCors({
     origin: 'http://localhost:3000',
