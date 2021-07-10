d2d052afdfc10b93f640e0a1823b4041fa79a831
Chaiwtun <chaiwtun@scg.com>
2021-07-10 22:17:23 +0700

    [main] simple sign-in with passport-local

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
+import { AuthController } from '~auth/auth.controller';
 import { AuthService } from '~auth/auth.service';
 import { LocalStrategy } from '~auth/local.strategy';
 import { UserModule } from '~user/user.module';
@@ -8,5 +9,6 @@ import { UserModule } from '~user/user.module';
 @Module({
   imports: [UserModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
