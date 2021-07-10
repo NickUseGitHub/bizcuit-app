177f5fdc00dbb48cbbb89942b99eb6af673efd7a
Chaiwtun <chaiwtun@scg.com>
2021-07-10 21:45:40 +0700

    [main] mock Auth & User modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { BeerModule } from '~beer/beer.module';
 import { VinesModule } from '~vine/vines.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BeerModule, VinesModule, AuthModule],
+  imports: [AuthModule, BeerModule, UserModule, VinesModule],
   providers: [],
 })
 export class AppModule {}
